99cc4ddc2a0c546230d97ff6d03940cce42503c9
Noah Ogrydziak <12644755+notarobot767@users.noreply.github.com>
2017-10-24 00:10:48 -0400

    remove dir since use dism

--- unmount_commit.bat
+++ unmount_commit.bat
@@ -1,6 +1,4 @@
 @ECHO OFF
-SET adk_dir="C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools"
 SET mount_dir="C:\WinPE_amd64\mount"
 
-CD %adk_dir%
 Dism /Unmount-Image /MountDir:%mount_dir% /commit
