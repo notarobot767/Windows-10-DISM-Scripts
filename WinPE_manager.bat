332ee2c92a79344d58efc671c4d2ac306e04c833
Noah Ogrydziak <12644755+notarobot767@users.noreply.github.com>
2017-11-03 16:32:33 -0400

    add createPEDrive

--- WinPE_manager.bat
+++ WinPE_manager.bat
@@ -1,16 +1,22 @@
+::tool to create, mount, commit, build ISO for WinPE
+
 @ECHO OFF
 COLOR F9
 
+:: where ADK is installed
+:: https://docs.microsoft.com/en-us/windows-hardware/get-started/adk-install
 SET adk_dir="C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\"
 SET adk_env="C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\DandISetEnv.bat"
-SET mount_dir=C:\WinPE_amd64\mount
-SET img_dir=C:\WinPE_amd64
-SET img_file=C:\WinPE_amd64\media\sources\boot.wim
-SET iso_file=WinPE.iso
-SET index=1
-
 CD %adk_dir%
 CALL %adk_env%
+
+::set the image directory
+SET img_dir=C:\WinPE_amd64
+SET choice=
+SET /P choice=enter image directory [%img_dir%]: 
+IF NOT '%choice%'=='' img_dir=%choice%
+ECHO using "%img_dir%" as image directory
+PAUSE
 
 :LOOP
 CLS
@@ -19,6 +25,8 @@ ECHO #   A. Mount an Image                 #
 ECHO #   B. Commit and Unmount an Image    #
 ECHO #   C. Discard and Unmount an Image   #
 ECHO #   D. Build an ISO                   #
+ECHO #   E. Create PE Drive                #
+ECHO #   Q. Quit                           #
 ECHO #######################################
 ECHO.
 
@@ -26,17 +34,23 @@ SET choice=
 SET /P choice=select option: 
 CLS
 
+:: /I makes the IF comparison case-insensitive
 IF /I '%choice%'=='A' GOTO MOUNT
 IF /I '%choice%'=='B' GOTO COMMIT
 IF /I '%choice%'=='C' GOTO DISCARD
 IF /I '%choice%'=='D' GOTO ISO
+IF /I '%choice%'=='E' GOTO CreatePEDrive
+IF /I '%choice%'=='Q' EXIT
 
 ECHO invalid input!
 PAUSE
 GOTO LOOP
 
 :MOUNT
-ECHO mounting...
+ECHO loading image and mounting at "%mount_dir"
+SET img_file=%img_dir%\media\sources\boot.wim
+SET mount_dir=%img_dir%\mount
+SET index=1
 CALL Dism /Mount-Image /ImageFile:%img_file% /index:%index% /MountDir:%mount_dir%
 PAUSE
 GOTO LOOP
@@ -55,6 +69,14 @@ GOTO LOOP
 
 :ISO
 ECHO building ISO...
+SET iso_file=WinPE_amd64.iso
 CALL MakeWinPEMedia /ISO %img_dir% %img_dir%\%iso_file%
+PAUSE
+GOTO LOOP
+
+:CreatePEDrive
+SET ark=amd64
+ECHO creating WinPE drive at "%img_dir%"
+CALL copype %ark% C:\WinPE_amd64
 PAUSE
 GOTO LOOP
