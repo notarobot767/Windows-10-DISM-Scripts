6dfcbf2f52e54e1751d158f28c53ea4af9fcc7d5
Noah Ogrydziak <12644755+notarobot767@users.noreply.github.com>
2017-11-07 21:49:31 -0500

    move pwedit to it's own bat file

--- tools/scripts/main.cmd
+++ tools/scripts/main.cmd
@@ -18,29 +18,14 @@ SET /P choice=select option:
 CLS
 
 :: /I makes the IF comparison case-insensitive
-IF /I '%choice%'=='A' GOTO DEPLOY
-IF /I '%choice%'=='B' GOTO PWedit
+IF /I '%choice%'=='A' CALL %scripts%\dism\deploy.cmd
+IF /I '%choice%'=='B' CALL %scripts%pwedit.bat
 IF /I '%choice%'=='Q' GOTO END
 IF /I '%choice%'=='R' GOTO REBOOT
 IF /I '%choice%'=='S' GOTO SHUTDOWN
 ECHO "%choice%" is not valid. Please try again.
 PAUSE
 GOTO Loop
-
-:: #############################################################
-:DEPLOY
-CLS
-CALL %scripts%\dism\deploy.cmd
-GOTO LOOP
-
-:: #############################################################
-:PWedit
-CLS
-ECHO Windows was in drive:
-FOR %%i in (C D E F G H I J K) do IF EXIST %%i:\Windows\System32\config\SAM ECHO %%i
-CALL %tools%\ntpwedit64\ntpwedit64
-PAUSE
-GOTO LOOP
 
 :: #############################################################
 :REBOOT
