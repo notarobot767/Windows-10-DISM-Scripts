b641f8ea107c78bd166cdd369db464812aa4390c
Noah Ogrydziak <notarobot767@users.noreply.github.com>
2017-11-26 22:06:47 -0500

    ommit writeout when applying

--- tools/scripts/deploy/apply_image.ps1
+++ tools/scripts/deploy/apply_image.ps1
@@ -1,6 +1,6 @@
 function applyHighPower {
   Write-Host "setting power scheme to High performance..."
-  powercfg /s $default_power_scheme | Write-Host
+  powercfg /s $default_power_scheme
 }
 
 function applyWriteDiskScript {
@@ -18,7 +18,7 @@ function applyWriteDiskScript {
 function applyFormatDisk {
   Write-Host "`nformatting the disk with Diskpart..."
   applyWriteDiskScript
-  diskpart /s $default_diskscript | Write-Host
+  diskpart /s $default_diskscript
 }
 
 function applyDISM {
@@ -26,14 +26,14 @@ function applyDISM {
   $wim_source = "{0}{1}" -f $default_wim_source, $default_wim_source_image
   "dism /apply-image /imagefile:{0} /index:{1} /applydir:{2}:\" -f
     $wim_source, $default_dism_index, $default_windows_drive |
-    Invoke-Expression | Write-Host
+    Invoke-Expression
 }
 
 function applyBCDBoot {
   Write-Host "`nwriting boot files with BCDBoot to system partition..."
   $mode = if($default_is_UEFI) { "UEFI" } else { "BIOS" }
   "bcdboot W:\Windows /s {0}: /f {1}" -f
-    $default_system_drive, $mode | Invoke-Expression | Write-Host
+    $default_system_drive, $mode | Invoke-Expression
 }
 
 function apply {
